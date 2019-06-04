ac63a79038cf510362f6e54d783e66fe678448c7
italofgcoura <33141562+italofgcoura@users.noreply.github.com>
2019-06-03 21:30:03 -0300

    alterações caixa

--- fenixFront/.js/caixa.js
+++ fenixFront/.js/caixa.js
@@ -1,7 +1,7 @@
 ﻿var caixa;
 var tipoUsuario;
 
-btnTipoTransacao.onClick = function () {
+btnTipoTransacao = function () {
     if (document.getElementById("entrada").checked == true) {
         document.getElementById("dropdownMenuButtonEntradas").disabled = false;
         document.getElementById("dataTransacaoEntrada").disabled = false;
@@ -31,4 +31,48 @@ window.onload = function () {
         },
         type: 'GET'
     });
+}
+
+
+
+listarHTML = function () {
+
+    for (var i = 0; i < caixa.length; i++) {
+
+        //dataValidade = estoque[i].dataValidade;
+        //if (dataValidade == null) {
+        //    dataValidade = '-'
+        //}
+
+        //else {
+        //    dataValidade = formataData(dataValidade);
+        //}
+
+
+        var valor;
+        if (caixa[i].tipoTransacao == 'entrada') {
+
+            valor = '<td style="color:green";>' + caixa[i].VALOR + '</td>'
+            
+        }
+        else if (caixa[i].tipoTransacao == 'saida') {
+            valor = '<td style="color:red";>' + caixa[i].VALOR + '</td>'   
+        }
+
+            var htmlString = '<tr>' +
+                      
+                '<td>' + caixa[i].ID_TRANSACAO + '</td>' +
+                '<td>' + formataData(caixa[i].DATA) + '</td>' +
+                '<td>' + caixa[i].DESCRICAO + '</td>'
+        + valor         
+            + '</tr>';
+        document.getElementById('tabela-caixa').innerHTML += htmlString;
+    }
+}
+
+
+formataData = function (dataFormatSQL) {
+    moment.locale('pt-br');
+    var dataPadraoBR = (moment(dataFormatSQL).format('DD/MM/YYYY'));
+    return dataPadraoBR
 }
